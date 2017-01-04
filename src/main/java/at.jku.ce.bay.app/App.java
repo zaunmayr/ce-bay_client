d4fa4744a010b4e964c2f7fa67ce07f7568ce503
zaunmayr <zaunmayr.susanne@gmx.at>
2017-01-04 13:02:06 +0100

    Add files via upload

---
+++ src/main/java/at.jku.ce.bay.app/App.java
@@ -0,0 +1,24 @@
+package at.jku.ce.bay.app;
+
+import akka.actor.ActorRef;
+import at.jku.ce.bay.helper.CEBayHelper;
+import java.io.File;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        //liefert den für 'Publish' benötigten Hashwert
+        CEBayHelper.GetHash(new File(""));
+
+        //Liefert die Referenz zum CEBay Actor, der vom CE Inst. bereitgestellt wird
+        CEBayHelper.GetRegistryActorRef();
+
+        //Wird für die Umwandlung einer ActorRef in die zu versendende Stringrepräsentation benötigt
+        CEBayHelper.GetRemoteActorRef(ActorRef.noSender());
+
+
+
+    }
+
+}
